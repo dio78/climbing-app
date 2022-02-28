df4ded5aa7fb9f23e588696f70658cfd62886a7f
Michael Grumbine <mgrumbine3@gmail.com>
2022-02-28 12:33:51 -0800

    fixed render capability

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import Header from './components/HeaderComponent.js';
+import LeafletComponent from './components/LeafletComponent';
+import Information from './components/Information';
 import { Container, Row, Col } from 'react-bootstrap';
 
 
@@ -7,7 +10,9 @@ function App() {
     <Container>
       <Row>
         <Col xs={{ span: 10, offset: 1}} className='text-center'>
-          
+          <Header />
+          <LeafletComponent />
+          <Information />
         </Col>
       </Row>
     </Container>
