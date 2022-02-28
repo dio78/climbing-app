f8f4a3e2a2e75475c5956ff3a80e3a3fa79d8d68
Dio <cdebon97@outlook.com>
2022-02-28 14:05:58 -0600

    Set up intial app.js render

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import { Container, Row, Col } from 'react-bootstrap';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Row>
+        <Col xs={{ span: 10, offset: 1}} className='text-center'>
+          <h1 className='m-5'>Climbing App</h1>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
