69e54ef890a6aa2164c989799fc610e6a9f51e30
Dio <cdebon97@outlook.com>
2022-02-28 15:53:03 -0600

    More minor additions to leaflet.js

--- src/components/LeafletComponent.js
+++ src/components/LeafletComponent.js
@@ -2,6 +2,7 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import './Leaflet.css'
 
 const LeafletComponent = () => {
+
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13}>
       <TileLayer
