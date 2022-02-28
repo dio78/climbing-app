004ede86aa281ab8661507401453c95363b7783b
Dio <cdebon97@outlook.com>
2022-02-28 15:58:29 -0600

    Successfully rendered polyline on leaflet map

--- src/components/LeafletComponent.js
+++ src/components/LeafletComponent.js
@@ -2,17 +2,30 @@ import { MapContainer, TileLayer, Marker, Popup, Polyline } from "react-leaflet"
 import '../Leaflet.css'
 
 const LeafletComponent = () => {
+
+  const polyline = [
+    [51.505, -0.09],
+    [51.51, -0.1],
+    [51.51, -0.12],
+  ]
+
   return (
     <MapContainer center={[51.505, -0.09]} zoom={13}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={polyline[0]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
       </Marker>
+      <Marker position={polyline[polyline.length - 1]}>
+        <Popup>
+          A pretty CSS3 popup. <br /> Easily customizable.
+        </Popup>
+      </Marker>
+      <Polyline pathOptions={{color: 'red'}} positions={polyline} />
     </MapContainer>
   )
 };
